3ce2e16580b7a84d4ca31dcfaed7abe5a13919db
alif dragon <alifjr763@gmail.com>
2023-03-30 15:50:53 +0800

    readme

---
+++ quiz1.js
@@ -0,0 +1,28 @@
+let message = "benr"
+//console.log(message)
+//console.log(message, typeof message)
+
+message = 123
+//console.log(message, typeof message)
+
+let str = "1.258"
+let num = 1
+
+let total = parseFloat(str) + num
+//console.log(total, typeof total)
+
+
+let data=["benr",3523,true]
+console.log(data[0])
+console.log(data[1])
+console.log(data[2])
+console.log(data[3])
+
+let user = {
+    name: "Hazim",
+    age: 55,
+    faculty: "FKEKK"
+}
+console.log(user.name)
+console.log(user.age)
+console.log(user.faculty)
